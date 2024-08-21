bac38a174a760dcfadd41c48f3a70ae6fa21144c
Gustavo Polli <gugalupolli@gmail.com>
2024-08-21 20:28:13 -0300

    ex

--- ferramenta-de-busca-ifpr-web/src/App.js
+++ ferramenta-de-busca-ifpr-web/src/App.js
@@ -1,12 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BackButton } from "./components/button/backButton";
+import { Button } from '@mui/base/Button';
+
 
 function App() {
   return (
     <div className="App">
       <div className="flex flex-col gap-2 w-10/12 bg-white mx-auto h-screen">
         <BackButton />
+        <Button className="bg-red-600 h-10 w-10"/>
+        <Button>
+          Teste botão
+        </Button>
       </div>
     </div>
   );
