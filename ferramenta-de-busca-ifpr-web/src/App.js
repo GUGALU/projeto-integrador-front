e96dbf324127d7fadd13769d6df3b84586e62517
Gustavo Polli <gugalupolli@gmail.com>
2024-08-21 20:22:10 -0300

    add react icons and mui

--- ferramenta-de-busca-ifpr-web/src/App.js
+++ ferramenta-de-busca-ifpr-web/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BackButton } from "./components/button/backButton";
 
 function App() {
   return (
     <div className="App">
-      <div className='bg-red-500 h-[100px]'>
+      <div className="flex flex-col gap-2 w-10/12 bg-white mx-auto h-screen">
+        <BackButton />
       </div>
     </div>
   );
---
+++ ferramenta-de-busca-ifpr-web/src/components/button/backButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { FaArrowLeft } from "react-icons/fa6";
+
+const BackButton = () => {
+  return (
+    <div className='flex border bg-blue-400 border-white text-white gap-2'>
+        {/* <FaArrowLeft /> */}
+        <h1>Voltar</h1>
+    </div>
+  )
+}
+
+export {BackButton}
