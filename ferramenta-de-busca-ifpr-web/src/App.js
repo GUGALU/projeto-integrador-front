cf528efdeed72796999c2306eab939e225805997
Gustavo Polli <gugalupolli@gmail.com>
2024-08-21 19:35:12 -0300

    add tailwind

--- ferramenta-de-busca-ifpr-web/src/App.js
+++ ferramenta-de-busca-ifpr-web/src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='bg-red-500 h-[100px]'>
+      </div>
     </div>
   );
 }
