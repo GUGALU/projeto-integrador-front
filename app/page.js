d928fedba1f449d6d5e5cd776b7fa549f40be9a1
gustavo.polli <gustavo.polli@fatecie.edu.br>
2024-09-25 13:58:18 -0300

    hahahahah

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import React from "react";
 import Search from "./components/search/search";
 import SearchCard from "./components/searchCard/searchCard";
-import Crud from "./components/crud/crud";
+import Crud from "./components/crud/CRUD";
 
 export default function Home() {
   return (
